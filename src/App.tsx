b7de5da6f7c0bf54cfd2d9507b47b86fc970991b
LeeChangHoon <changhoon1030@gmail.com>
2022-11-05 17:00:24 +0900

    ⭐ feat : GlobalStyle, Router, RecoilRoot 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Router from "./Router";
+import GlobalStyle from "./styles/GlobalStyle";
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <RecoilRoot>
+        <GlobalStyle/>
+        <Router />
+      </RecoilRoot>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import {Route, Routes, Navigate} from 'react-router-dom';
+import DashBoard from './pages/DashBoard';
+
+
+const Router = () => {
+  return(
+    <Routes>
+      <Route path='/' element={<DashBoard />} />
+      <Route path='/admanagement' element={<DashBoard />} />
+      <Route path='*' element={<Navigate replace to='/' />} />
+    </Routes>
+  )
+}
+export default Router
