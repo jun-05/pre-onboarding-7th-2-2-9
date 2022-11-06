5df389e29672b2a7b528b4342958b12b454ea21a
LeeChangHoon <changhoon1030@gmail.com>
2022-11-07 04:19:07 +0900

    ⭐ feat : AdManageMentContent 기능 구현

--- src/components/button/BasicBtn.jsx
+++ src/components/button/BasicBtn.jsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
-const BasicBtn = () => {
-  return <Btn />;
+const BasicBtn = ({ name, color, backgrounColor, border }) => {
+  return (
+    <Btn color={color} backgrounColor={backgrounColor} border={border}>
+      {name}
+    </Btn>
+  );
 };
 
-const Btn = styled.button``;
+const Btn = styled.button`
+  background-color: ${props => props.backgrounColor};
+  padding: 1.2rem 2rem;
+  border: ${props => props.border};
+  border-radius: 1rem;
+  font-weight: 700;
+  line-height: 1.6rem;
+  color: ${props => props.color};
+`;
 
 export default BasicBtn;
--- src/components/content/AdManagementContent/AdManagementContent.jsx
+++ src/components/content/AdManagementContent/AdManagementContent.jsx
@@ -1,14 +1,71 @@
-import axios from "axios"
+import styled from 'styled-components';
+import { convertIsoToTimeStamp } from '../../../utils/convertFn';
+import BasicBtn from '../../button/BasicBtn';
 
-const AdManagementContent = () => {
-  const fetchData = async () => {
-    const response = await axios.get('/server/wanted_FE_ad-list-data-set.json')
-    console.log(response)
-  }
-  fetchData()
-  return( 
-    <div></div>
-  )
-}
+const AdManagementContent = ({ adData }) => {
+  const convertedDate = convertIsoToTimeStamp(adData.startDate);
+  return (
+    <AdContentContainer>
+      <AdTitle>{adData.title}</AdTitle>
+      <AdAttributeWrapper>
+        <LeftItem>상태</LeftItem>
+        <RightItem>{adData.status}</RightItem>
+      </AdAttributeWrapper>
+      <AdAttributeWrapper>
+        <LeftItem>광고 생성일</LeftItem>
+        <RightItem>{convertedDate}</RightItem>
+      </AdAttributeWrapper>
+      <AdAttributeWrapper>
+        <LeftItem>일 희망 예산</LeftItem>
+        <RightItem>{adData.budget}</RightItem>
+      </AdAttributeWrapper>
+      <AdAttributeWrapper>
+        <LeftItem>광고 수익률</LeftItem>
+        <RightItem>{adData.report.roas}</RightItem>
+      </AdAttributeWrapper>
+      <AdAttributeWrapper>
+        <LeftItem>매출</LeftItem>
+        <RightItem>{(adData.report.roas * adData.report.cost) / 100} </RightItem>
+      </AdAttributeWrapper>
+      <AdAttributeWrapper>
+        <LeftItem>광고 비용 </LeftItem>
+        <RightItem>{adData.report.cost}</RightItem>
+      </AdAttributeWrapper>
+      <BasicBtn
+        name={'수정하기'}
+        color={'#3A474E'}
+        backgrounColor={'#ffffff'}
+        border={'1px solid #D1D8DC'}
+      />
+    </AdContentContainer>
+  );
+};
 
-export default AdManagementContent
+const AdContentContainer = styled.section`
+  border: 1px solid #d1d8dc;
+  border-radius: 1rem;
+  padding: 5%;
+  width: 30rem;
+`;
+
+const AdTitle = styled.div`
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  margin: 3% 0;
+`;
+const AdAttributeWrapper = styled.section`
+  display: flex;
+  border-bottom: 1px solid #edeff1;
+  font-size: 1.2rem;
+  font-weight: 700;
+  padding: 3% 0;
+`;
+
+const LeftItem = styled.div`
+  width: 30%;
+  color: #94a2ad;
+`;
+const RightItem = styled.div``;
+
+export default AdManagementContent;
