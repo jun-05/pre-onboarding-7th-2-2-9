cc67c3f324918b2bdf7d444772bd0913df4a34b1
LeeChangHoon <changhoon1030@gmail.com>
2022-11-07 04:17:17 +0900

    ⭐ feat : svg Icon 추가

--- src/assets/svg/DropDownIcon.jsx
+++ src/assets/svg/DropDownIcon.jsx
@@ -2,7 +2,10 @@ const DropDownIcon = () => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_3358_44)">
-        <path d="M7.41 8.58997L12 13.17L16.59 8.58997L18 9.99997L12 16L6 9.99997L7.41 8.58997Z" fill="#3A474E" />
+        <path
+          d="M7.41 8.58997L12 13.17L16.59 8.58997L18 9.99997L12 16L6 9.99997L7.41 8.58997Z"
+          fill="#3A474E"
+        />
       </g>
       <defs>
         <clipPath id="clip0_3358_44">
@@ -10,7 +13,7 @@ const DropDownIcon = () => {
         </clipPath>
       </defs>
     </svg>
-  )
-}
+  );
+};
 
-export default DropDownIcon
+export default DropDownIcon;
